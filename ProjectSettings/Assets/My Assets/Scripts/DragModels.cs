6c57950b4d124c7acab11cf27a74216bc2190b2d
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2016-11-22 13:59:02 -0200

    Versão 0.1

---
+++ ProjectSettings/Assets/My Assets/Scripts/DragModels.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragModels : MonoBehaviour {
+
+	private float distance;
+	private bool dragging = false;
+	private bool rotating = false;
+	private Vector3 offset;
+	private Transform toDrag;
+
+	public float sensitivity = 1f;
+
+	private bool editorDragging = false;
+	private bool editorRotating = false;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		Vector3 v3;
+	
+		//Se o número de toques for menor que 1 saia do Update
+		if (Input.touchCount < 1) {
+			dragging = false;
+			return;
+		}
+
+		//Se a contagem de touches for igual a 1
+		//Executar código para arrastar objetos
+		if (editorDragging) {
+			Touch touch = Input.touches [0];
+			Vector3 pos = touch.position;
+
+			if (touch.phase == TouchPhase.Began) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (pos);
+
+				if (Physics.Raycast (ray, out hit) && (hit.collider.tag == "Dragable")) {
+					print ("Dragging");
+					toDrag = hit.collider.transform;
+
+					distance = hit.transform.position.z - Camera.main.transform.position.z;
+					v3 = new Vector3 (pos.x, pos.y, distance);
+					v3 = Camera.main.ScreenToWorldPoint (v3);
+					offset = toDrag.position - v3;
+					dragging = true;
+				}
+			}
+			if (dragging && touch.phase == TouchPhase.Moved) {
+				print ("touch phase moved");
+				v3 = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+				v3 = Camera.main.ScreenToWorldPoint (v3);
+				toDrag.position = v3 + offset;
+			}
+			if (dragging && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
+				print ("touch phase ended canceled");
+				dragging = false;
+			}
+		}
+
+		//Se a contagem de touches for igual a 2
+		//Executar código para girar objetos
+		if (editorRotating) {
+			Touch touch = Input.touches [0];
+			Vector3 pos = touch.position;
+
+			if (touch.phase == TouchPhase.Began) {
+				RaycastHit hit;
+				Ray ray = Camera.main.ScreenPointToRay (pos);
+
+				if (Physics.Raycast (ray, out hit) && (hit.collider.tag == "Dragable")) {
+					print ("Rotating");
+					toDrag = hit.collider.transform;
+					distance = hit.transform.position.z - Camera.main.transform.position.z;
+					v3 = new Vector3 (pos.x, pos.y, distance);
+					v3 = Camera.main.ScreenToWorldPoint (v3);
+					offset = toDrag.position - v3;
+					rotating = true;
+				}
+
+			}
+			if (rotating && touch.phase == TouchPhase.Moved) {
+				v3 = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+				v3 = Camera.main.ScreenToWorldPoint (v3);
+				offset = toDrag.position - v3;
+
+				Vector3 angles = new Vector3 (-0, offset.x, 0);
+			
+
+				toDrag.transform.eulerAngles += angles * sensitivity;
+			}
+			if (rotating && (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)) {
+				print ("touch phase ended canceled");
+				rotating = false;
+			}
+		}
+	}
+
+	public void EditorDragging(){
+		editorDragging = !editorDragging;
+	}
+	public void EditorRotating(){
+		editorRotating = !editorRotating;
+	}
+}
+
---
+++ ProjectSettings/Assets/My Assets/Scripts/UIButtons.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour {
+
+	public GameObject modelHolder;
+
+	public GameObject plant01;
+	public GameObject plant02;
+
+	bool isMenuImagesActive;
+
+	public void ResetScene (){
+		SceneManager.LoadScene (0);
+	}
+
+
+	//UI Menu
+	public void DrowDownMenu () {
+		foreach (Transform child in transform) {
+			if (isMenuImagesActive) {
+				child.gameObject.SetActive (false);
+			} else {
+				child.gameObject.SetActive (true);
+			}
+		}
+
+		isMenuImagesActive = !isMenuImagesActive;
+	}
+
+	public void Planta01(){
+		//Destrua todos os filhos do ModelHolder
+		foreach (Transform child in modelHolder.transform) {
+			Destroy (child.gameObject);
+		}
+		Instantiate (plant01, modelHolder.transform,true);
+	}
+
+	public void Planta02(){
+		//Destrua todos os filhos do ModelHolder
+		foreach (Transform child in modelHolder.transform) {
+			Destroy (child.gameObject);
+		}
+		Instantiate (plant02, modelHolder.transform,true);
+	}
+}
